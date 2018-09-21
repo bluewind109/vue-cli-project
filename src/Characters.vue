e937978da5a1d90731176d4b5d981aba0383df75
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2018-09-21 09:50:29 +0700

    Try global and local components import

---
+++ src/Characters.vue
@@ -0,0 +1,22 @@
+<template>
+<ul>
+  <li v-for="character in characters">{{character}}</li>
+</ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      characters: ['Khan', 'Dzu', 'Yi']
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+// Global components
+// import Characters from './Characters.vue'
+//
+// Vue.component('characters', Characters);
 
 new Vue({
   el: '#app',
