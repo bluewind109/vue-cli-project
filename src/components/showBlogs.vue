e91281954ac66f9d10ed27613705e307716888ea
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2018-09-26 16:44:50 +0700

    Route param

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -3,7 +3,9 @@
   <h1>All Blog Articles</h1>
   <input type="text" v-model="search" placeholder="search blogs">
   <div v-for="blog in filteredBlogs" class="single-blog">
-    <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
+    <router-link :to="'/blog/' + blog.id">
+      <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
+    </router-link>
     <p>{{blog.body | snippet}}</p>
   </div>
 </div>
