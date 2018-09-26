fdc1ca1d57ec9614b4c386c0ef93159135c789b4
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2018-09-26 15:37:21 +0700

    Mixin

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
 <template>
 <div>
   <show-blogs></show-blogs>
+  <list-blogs></list-blogs>
 </div>
 </template>
 
 <script>
-import addBlog from './components/addBlog.vue'
-import showBlogs from './components/showBlogs.vue'
+import addBlog from './components/addBlog.vue';
+import showBlogs from './components/showBlogs.vue';
+import listBlogs from './components/listBlogs.vue';
 
 export default {
   //Local components
   components: {
     'add-blog': addBlog,
-    'show-blogs': showBlogs
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs
   },
   data() {
     return {}
